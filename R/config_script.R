4a9920ea2c59a2559f68bd4b8ad74e5daaed8121
Kees van Eijden <k.vaneijden@gmail.com>
2021-02-15 21:29:10 +0100

    WIP 1

---
+++ R/config_script.R
@@ -0,0 +1,32 @@
+#' Config RStudio workspace in Research Cloud
+#'
+#'
+config_workspace <- function() {
+
+  github <- rstudioapi::showQuestion(title = 'GITHUB access', message = 'Store GitHub PAT?', ok = "Yes", cancel = 'No')
+  if(github) {
+    credentials::set_github_pat(force_new = TRUE)
+  }
+
+  # set .Rprofile
+  #
+  rprofile <- rstudioapi::showQuestion(title = 'Rprofile', message = 'Set PAT each session?', ok = "Yes", cancel = 'No')
+  if(rprofile) {
+    rprofile_name <- file.path(Sys.getenv('HOME'), '.Rpprofile')
+    rprofile_cmd <- c(
+      "# Set github PAT for session (added by config_workspace)",
+      "credentials::set_github_pat()",
+      ""
+    )
+    write(x = rprofile_cmd, file = rprofile_name, append = TRUE)
+    Sys.chmod(paths = rprofile_name, mode = '0600')
+  }
+
+  # install icommands
+  #
+  icommands <- rstudioapi::showQuestion(title = 'Yoda', message = 'Install icommands?', ok = "Yes", cancel = 'No')
+  if (icommands) {
+    install_script <- file.path(getwd(), 'inst/install_icommands.sh')
+    system2(command = 'sh', args = install_script, stdout = 'ii.out', stderr = 'ii.err')
+  }
+}
