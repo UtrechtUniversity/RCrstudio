f0bf26394cbe416378afc5e01167e7003661a058
k.vaneijden <k.vaneijden@uu.nl>
2021-02-28 16:51:49 +0000

    WIP 02-28 sync_data

--- R/sync_data.R
+++ R/sync_data.R
@@ -5,18 +5,20 @@
 #'
 #' @param project Name (character string) of the project
 #' @param check Type of check to perform before synchronizing. See Details
-#' @param delete What to do with files in target not in source anymore. If FALSE (default) those files remain in target unchanged.
-#' If TRUE they are removed.
+#' @param delete What to do with files in target directory which are  not in source collection anymore.
+#' If FALSE (default) those files remain in target directory, else they are removed.
+#' @param ... path to a subcollection within collection
 #'
+#' @return
+#' If delete is FALSE, a list of files in target which aren't in source collection anymore. If TRUE list of removed files.
 #' @export
 #'
-sync_data  <- function(project = NULL, sub = NULL, delete = FALSE, check = 'hash') {
+sync_data  <- function(project = NULL, collection = NULL, delete = FALSE, ...) {
 
   # read Yoda home from json file
   irods_env_json <- file.path(Sys.getenv('HOME'), '.irods', 'irods_environment.json')
   if(!file.exists(irods_env_json)) {
-    warning(sprintf('No irods environment file %s', irods_env))
-    return(FALSE)
+    stop(sprintf('No irods environment file %s', irods_env))
   }
   yoda_home <- fromJSON(txt = irods_env_json)$irods_home
 
@@ -24,24 +26,22 @@ sync_data  <- function(project = NULL, sub = NULL, delete = FALSE, check = 'hash
   #
   irods_not_connected <- system2('ils', stdout = FALSE, stderr = FALSE)
   if (irods_not_connected) {
-    warning('No irods connection')
-    return(FALSE)
+    stop('No irods connection')
   }
 
   # construct collection path out of yoda home, project name and subproject name
   if (is.null(project)) {
-    warning('Project name is NULL')
-    return(FALSE)
+    stop('Project name is NULL')
   }
   collection_path <- file.path(yoda_home, project, 'data')
-  if(!is.null(sub)) {
-    collection_path <- file.path(collection_path, sub)
+  if(!is.null(collection)) {
+    collection_path <- file.path(collection_path, collection, ...)
   }
 
   # test existence of data collection in Yoda
   irods_value <- system2(command = 'icd', args = collection_path, stdout = FALSE, stderr = FALSE)
   if (irods_value != 0) {
-    warning(sprintf('%s: Yoda collection does not exists', collection_path))
+    stop(sprintf('%s: Yoda collection does not exists', collection_path))
     return(FALSE)
   }
 
@@ -49,45 +49,41 @@ sync_data  <- function(project = NULL, sub = NULL, delete = FALSE, check = 'hash
   #
   local_project<- basename(getwd())
   if (local_project != project) {
-    warning("%s: working directory not project directory", getwd())
-    retrun(FALSE)
+    stop("%s: working directory not project directory", getwd())
   }
 
   local_data <- file.path(getwd(), 'data')
-  if (!is.null(sub)) {
-    local_data <- file.path(local_data, sub)
+  if (!is.null(collection)) {
+    local_data <- file.path(local_data, collection, ...)
   }
   if (!dir.exists(local_data)) {
-    warning(sprintf("%s: local data collection doesn't exists", local_data))
+    stop(sprintf("%s: local data collection doesn't exists", local_data))
   }
 
   # make irsync command
   source      <- sprintf("i:%s", collection_path)
   destination <- local_data
   irsync_args <- c('-r', source, destination)
-  #print(sprintf('irsync %s %s %s %s', irsync_args[1], irsync_args[2], irsync_args[3], irsync_args[4]))
   irsync_value <- system2(command = 'irsync', args = irsync_args, stdout = FALSE, stderr = FALSE)
   if (irsync_value != 0) {
-    warning(sprintf("Error while executing: 'irsync %s'", irsync_args))
-    return(FALSE)
-  }
-
-  if (!delete) {
-    return(TRUE)
+    stop(sprintf("Error while executing: 'irsync %s'", irsync_args))
   }
 
   # look for left-overs in destination
   #
-  irsync_args  <- c('-r', '-l', destination, source)
-  irsync_list <- system2(command = 'irsync', args = irsync_args, stdout = TRUE, stderr = TRUE)
+  irsync_args <- c('-r', '-l', destination, source)
+  irsync_list <- system2(command = 'irsync', args = irsync_args, stdout = TRUE, stderr = TRUE) # Find left overs by a dry run of a reverse sync
   if(length(irsync_list) > 1) {
-    left_overs  <- get_left_overs(irsync_list)
+    left_overs  <- get_filenames_irsync(irsync_list)
+  } else {
+    left_overs <- NULL
   }
 
+  if (delete && !is.null(left_overs)) {
+    file.remove(left_overs)
+  }
 
-
-
-  return(irsync_value)
+  return(left_overs)
 
   #
 
@@ -97,8 +93,24 @@ sync_data  <- function(project = NULL, sub = NULL, delete = FALSE, check = 'hash
 
 }
 
-get_left_overs <- function(irsync_list) {
-  l <- map(.x = out, .f = function(x, pattern) str_split(string = x, pattern = pattern)[[1]], pattern = '   ')
-  m <- reduce(.x = l, .f = function(x, y) c(x, y[[1]]))
+#' Get Filenames from irsync List
+#'
+#' @details
+#' Each line (except first) has three parts seperated by 3 spaces: filename, byte count, and ?
+#  The first line is a general message and can be discarded
+#'
+#'
+#' @param irsync_list Output text (vector of strings) of \emph{irsync -l}
+#'
+get_filenames_irsync <- function(irsync_list) {
+  irsync_list <- irsync_list[2:length(irsync_list)]
 
+  # get first part of each line in a list and convert to vector
+  #
+  l <- map_chr(.x = irsync_list, .f = function(x) str_split(string = x, pattern = '   ')[[1]][[1]])
+  l
+}
+
+construct_file_path <- function(...) {
+  file.path(getwd(), ...)
 }
